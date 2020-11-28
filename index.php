1eeaaecb59a8c460413d6e736c55076e3bb0c7bc
varun.elavia@dkatalis.com <varun.elavia@dkatalis.com>
2020-11-28 11:11:04 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+    $db_host = "";
+    $db_username = "";
+    $db_password = "";
+    $db_dbname = "";
+    $db_port = 3306;
+    $db_conn = mysqli_connect($db_host, $db_username, $db_password, $db_dbname, $db_port);
+    
+    #mysqli_query($db_conn, query)
+    $num1 = $_REQUEST["num1"];
+    $num2 = $_REQUEST["num2"];
+    $operator = $_REQUEST["operator"];
+    if($operator == "+")
+    {
+        $result = $num1 + $num2;
+    }
+    elseif($operator == "-")
+    {
+        $result = $num1 - $num2;
+    }
+    elseif($operator == "*")
+    {
+        $result = $num1 * $num2;
+    }
+    elseif($operator == "/")
+    {
+        $result = $num1 / $num2;
+    }
+    else
+    {
+        $result = "Invalid Operator";
+    }
+    print("Result is ="+$result);
+?>
+<form action="index.php" method="POST">
+     Num1: <input type="number" name="num1">
+     Num2: <input type="number" name="num2">
+     Operator: <input type="number" name="operator">
+     <input type="submit">
+</form>
