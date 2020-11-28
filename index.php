3755453685b640a4c761a6e63c744269357163d7
varun.elavia@dkatalis.com <varun.elavia@dkatalis.com>
2020-11-28 11:46:50 +0530

    first commit

--- index.php
+++ index.php
@@ -5,13 +5,6 @@
      <input type="submit">
 </form>
 <?php
-    #$db_host = "";
-    #$db_username = "";
-    #$db_password = "";
-    #$db_dbname = "";
-    #$db_port = 3306;
-    #$db_conn = mysqli_connect($db_host, $db_username, $db_password, $db_dbname, $db_port);
-    #mysqli_query($db_conn, query)
     $num1 = $_REQUEST["num1"];
     $num2 = $_REQUEST["num2"];
     $operator = $_REQUEST["operator"];
